6c6ffad6a4851fdb4a956d19f7f1b0c775eac942
matthewkuangga <47339372+davrminator@users.noreply.github.com>
2023-08-31 16:56:07 -0700

    image screen done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
 import { useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [query, setQuery] = useState("")
+  const [data, setData] = useState({})
+
+  const search = () => {
+    axios.get(`https://pixabay.com/api/?key={}&q=${query}`)
+      .then((res) => setData(res.data))
+      .catch((err) => console.error(err))
+  }
 
   return (
     <div style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection:"column", height: "100vh", width:"100vw"}}>
-      {/* <a href="https://vitejs.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a> */}
       <h1 style={{margin: "10px"}}>Pixabay Image Search</h1>
-      <div style={{marginBottom: "10px"}}>
-        <input style={{minWidth:"250px", marginRight: "10px"}} placeholder='Search for an image...'/>
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div style={{marginBottom: "20px"}}>
+        <input style={{minWidth:"250px", marginRight: "10px"}} placeholder='Search for an image...' value={query} onChange={(e) => setQuery(e.target.value)}/>
+        <button onClick={() => search()}>
           Search
         </button>
       </div>
-      <div style={{background: "red", height:"70vh", width:"80vw", minWidth:"350px"}}>
-        grid of pictures go here
+      <div style={{borderRadius: "8px", height:"70vh", width:"80vw", minWidth:"350px", display:"flex", justifyContent: "space-around", alignItems:"center", flexWrap: "wrap"}}>
+        {data.hits && data.hits.map((item) => {
+          return <img key={item.previewURL} src={item.previewURL} alt={item.tags} style={{borderRadius: "8px", background: "white"}}/>
+        })}
       </div>
     </div>
   )
